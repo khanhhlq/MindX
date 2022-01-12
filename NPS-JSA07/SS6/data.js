f3263b9c374e72fb9e8229fb82b0d47b5333cbb9
ooAkira <lee.quockhanh1107@gmail.com>
2022-01-12 19:30:29 +0700

    done setup ss6

---
+++ NPS-JSA07/SS6/data.js
@@ -0,0 +1,7 @@
+let menu = [
+    {
+        name: "Pho",
+        title: "Đay la mon an co truyen Ha Noi va Viet Nam",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium quaerat, incidunt dolorum quod nisi unde autem. Labore atque necessitatibus maxime sit, deleniti inventore quaerat praesentium porro facere exercitationem, sunt officiis?"
+    }
+]
