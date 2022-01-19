db9ef76b391aae85a0b6fca6c0fc422cbcabfbb1
ooAkira <lee.quockhanh1107@gmail.com>
2022-01-19 19:55:49 +0700

    done arrow function

--- NPS-JSA07 (Nâng cao)/SS7/app.js
+++ NPS-JSA07 (Nâng cao)/SS7/app.js
@@ -16,6 +16,31 @@ console.log(a+ " " + b)
             var con = "Đây là biến con được khái báo ở khối 3";
         }
     }
-    console.log("cha: ", cha); // Sẽ bị lỗi: vì nằm ở ngoài block
+    // console.log("cha: ", cha); // Sẽ bị lỗi: vì nằm ở ngoài block
 }
 console.log("con: ", con);
+// Lưu ý: khi khai báo biến: nên khai báo bằng let.
+
+// 3. Arrow Function
+// Các khai báo hàm bình thường
+let sum = function sum(a, b){
+    return a + b;
+}
+
+console.log("sum(1,2): ", sum(1,2));
+
+// Arrow Function thì như sau:
+
+const sum2 = (a, b) =>{
+    return a + b;
+}
+
+console.log("sum2(3,4): ", sum2(3,4));
+
+// Cú pháp tổng quát
+
+/*
+const tenHam = (parmeters, ...) =>{
+    Code JS
+}
+*/
