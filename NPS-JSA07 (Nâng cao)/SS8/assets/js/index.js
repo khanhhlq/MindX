4af8b7998b22b3086f0cd7395bbddac7feb4f263
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-23 19:30:46 +0700

    Done css và html

---
+++ NPS-JSA07 (Nâng cao)/SS8/assets/js/index.js
@@ -0,0 +1,9 @@
+const searchInput = document.querySelectorAll("#search-input");
+const cityName = document.querySelector(".city-name");
+const weatherState = document.querySelector(".weather-state");
+const weatherIcon = document.querySelector(".weather-icon");
+const temperature = document.querySelector(".temperature");
+
+fetch("")
+.then
+.catc
