8f9f6d93b966ae336dd3ddd407016144fcbb0cfb
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-23 20:17:20 +0700

    Done khai bao bien

--- NPS-JSA07 (Nâng cao)/SS8/assets/js/index.js
+++ NPS-JSA07 (Nâng cao)/SS8/assets/js/index.js
@@ -1,9 +1,15 @@
+const API_KEY = '4269652c5e1b6a66a34f9d3d16ecc098';
 const searchInput = document.querySelectorAll("#search-input");
 const cityName = document.querySelector(".city-name");
 const weatherState = document.querySelector(".weather-state");
 const weatherIcon = document.querySelector(".weather-icon");
 const temperature = document.querySelector(".temperature");
+const sunrise = document.querySelector(".sunrise");
+const sunset = document.querySelector(".sunset");
+const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".wind-speed");
 
-fetch("")
-.then
-.catc
+
+// fetch(`api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`)
+//     .then(data => console.log(data))
+
