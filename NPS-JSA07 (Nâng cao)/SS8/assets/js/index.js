6a6885cf39f9ce6a8c3be68ee836be858e77c0a9
ooAkira <lee.quockhanh1107@gmail.com>
2022-02-23 20:58:07 +0700

    Done js

--- NPS-JSA07 (Nâng cao)/SS8/assets/js/index.js
+++ NPS-JSA07 (Nâng cao)/SS8/assets/js/index.js
@@ -1,5 +1,6 @@
 const API_KEY = '4269652c5e1b6a66a34f9d3d16ecc098';
-const searchInput = document.querySelectorAll("#search-input");
+const DEFAULT_VALUE = '--';
+const searchInput = document.getElementById("search-input");
 const cityName = document.querySelector(".city-name");
 const weatherState = document.querySelector(".weather-state");
 const weatherIcon = document.querySelector(".weather-icon");
@@ -9,7 +10,22 @@ const sunset = document.querySelector(".sunset");
 const humidity = document.querySelector(".humidity");
 const windSpeed = document.querySelector(".wind-speed");
 
+searchInput.addEventListener("change", (event) => {
+    console.log(event.target.value);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${API_KEY}&lang=vi&units=metric`)
+        .then(response => response.json())
+        .then((data) => {
+            cityName.innerHTML = data.name || DEFAULT_VALUE;
+            weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+            weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` || DEFAULT_VALUE;
+            temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
 
-// fetch(`api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`)
-//     .then(data => console.log(data))
+            sunrise.innerHTML = moment.unix(data.sys.sunrise).format('HH:mm') || DEFAULT_VALUE;
+            sunset.innerHTML = moment.unix(data.sys.sunset).format('HH:mm') || DEFAULT_VALUE;
+            humidity.innerHTML = data.main.humidity || DEFAULT_VALUE;
+            windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
+        })
+})
+
+
 
