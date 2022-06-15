62b245bd2ff22f830c2f69afa6d58fca0d3312ff
ooAkira <lee.quockhanh1107@gmail.com>
2022-06-15 15:53:51 +0700

    📁 Add folder - my-food

---
+++ my-food/app.js
@@ -0,0 +1,7 @@
+const btnOpen = () => {
+    document.getElementById("mySidebar").style.display = "flex";
+}
+
+const btnClose = () => {
+    document.getElementById("mySidebar").style.display = "none";
+}
