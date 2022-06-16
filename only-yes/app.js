9ab57b919750c30b850a8aa324d825525d038abe
ooAkira <lee.quockhanh1107@gmail.com>
2022-06-16 09:38:13 +0700

    🤡 Fix bug

--- only-yes/app.js
+++ only-yes/app.js
@@ -2,3 +2,8 @@ document.getElementById("btnNo").addEventListener("mousemove", () => {
     document.getElementById("btnNo").innerHTML = "Trải nghiệm Ngay"
     document.getElementById("btnYes").innerHTML = "Xem thêm khoá học"
 })
+
+document.getElementById("btnYes").addEventListener("mousemove", () => {
+    document.getElementById("btnYes").innerHTML = "Trải nghiệm ngay"
+    document.getElementById("btnNo").innerHTML ="Xem thêm khoá học"
+})
